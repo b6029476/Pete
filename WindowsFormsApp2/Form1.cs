bfdb09d214c226f1569b054a5c3e7faa7c628c66
b6029476 <b6029476@my.shu.ac.uk>
2017-04-27 17:30:01 +0100

    open

--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -91,12 +91,15 @@ namespace WindowsFormsApp2
 
         private void openToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            OpenFileDialog theDialog = new OpenFileDialog();
-            theDialog.Title = "Open Text File";
-            theDialog.Filter = "TXT files|*.txt";
-            theDialog.InitialDirectory = @"C:\";
-            if (theDialog.ShowDialog() == DialogResult.OK)
-            {
+            Stream myStream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {// http://stackoverflow.com/questions/13900441/c-sharp-read-txt-file-into-textbox
                 richTextBox1.Text = File.ReadAllText(openFileDialog1.FileName);
             }
         }
